c1fbbf0f46ef8f09199cd3e3499a645999f25f6a
Muhammad Umar Waqar <mohammthegreat@gmail.com>
2025-03-28 05:46:27 +0500

    Adding next Fixes

--- next.config.js
+++ next.config.js
@@ -7,7 +7,16 @@ const nextConfig = {
     locales: ['en', 'es'],
     defaultLocale: 'en',
     localeDetection: false,
-    
+    domains: [
+      {
+        domain: 'dediabetes.com',
+        defaultLocale: 'en',
+      },
+      {
+        domain: 'es.dediabetes.com',
+        defaultLocale: 'es',
+      },
+    ],
   },
   images: {
     domains: ['images.prismic.io', 'dediabetes.cdn.prismic.io'],
@@ -74,6 +83,7 @@ const nextConfig = {
         source: '/acido-linoleico-conjugado',
         destination: '/es/blog/acido-linoleico-conjugado',
         permanent: true,
+        locale: false,
       },
       {
         source: '/acido-lipoico-diabetes',
